3724f4fc3131a6952ff2c291925aca4f5a09d011
vg <vitor.matoso.barros@escola.pr.gov.br>
2026-06-26 08:41:51 -0300

    Update exercicios.js

--- exercicios.js
+++ exercicios.js
@@ -1,4 +1,14 @@
-let vida = 100 
-let heroi = "BRUCE WAYNE"
-heroi = "BATMAN"
-let login = true 
+let vida = 100; 
+let heroi = "BRUCE WAYNE"; 
+heroi = "BATMAN"; 
+let login = true; 
+let nomedaescola = "Colégio Estadual"; 
+let nivelAtual = 100; 
+console.log("inicializando sistema")
+
+console.log("O nível atual: ", nivelAtual);
+let nivelatual = 77
+let poder = 34
+poder = 90
+
+console.log("seu poder atual é de:", poder)
